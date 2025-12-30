ec7034c3b8f5fd77b9774ee1bd49528b2f6476ac
Devendra Saini <30dsaini@gmail.com"  git config --global user.email 30dsaini@gmail.com  git config --global user.email '30dsaini@gmail.com>
2025-12-30 10:06:47 +0530

    icon changr  and  use image

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="  text-gray sticky top-0 z-50 bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 ">
+    <nav className="  text-cyan-800 sticky top-0 z-50 bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 ">
       <div className="max-w-6xl mx-auto  py-3 flex items-center justify-between ">
         
         {/* Logo */}
@@ -14,19 +14,19 @@ function Navbar() {
 
         {/* Desktop Menu  */}
         <div className=" text-xl hidden md:flex space-x-10">
-          <a href="#home" className="hover:text-cyan-500 transition ">
+          <a href="#home" className="hover:text-cyan-500 transition hover:underline decoration-cyan-500 ">
             Home
           </a>
-          <a href="#about" className="hover:text-cyan-500 transition ">
+          <a href="#about" className="hover:text-cyan-500 transition  hover:underline decoration-cyan-500">
             About
           </a>
-           <a href="#skill" className="hover:text-cyan-500 transition ">
+           <a href="#skill" className="hover:text-cyan-500 transition hover:underline decoration-cyan-500">
          Skills
           </a>
-          <a href="#projects" className="hover:text-cyan-500 transition ">
+          <a href="#projects" className="hover:text-cyan-500 transition hover:underline decoration-cyan-500">
             Projects
           </a>
-          <a href="#contact" className=  " hover:text-cyan-500 transition ">
+          <a href="#contact" className=  " hover:text-cyan-500 transition hover:underline decoration-cyan-500">
             Contact
           </a>
         </div>
